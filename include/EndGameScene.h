1ae90f5b440613c37d5b6529033d31bca33da284
Alejandro Moltó <alejandromolton@gmail.com>
2025-11-27 20:57:12 +0100

    implemented class EndGameScene.

--- include/EndGameScene.h
+++ include/EndGameScene.h
@@ -1,13 +1,19 @@
-#ifndef EXITSCENE_H
-#define EXITSCENE_H
+#ifndef ENDGAMESCENE_H
+#define ENDGAMESCENE_H
 #include "Scene.h"
 #include "board.h"
 
 
 class EndGameScene : public Scene {
+private:
+    // There is two variables instead of one because the situation of stalemate (nobody won) is possible in chess.
+    bool whiteWon; 
+    bool blackWon;
 public:
     using Scene::Scene;
+    EndGameScene(Scene* lastScene, SDL_Renderer* renderer, int width, int height, std::string filename, bool whiteWon, bool blackWon);
     Scene* HandleEvent(SDL_Point click) override;
+    void render() override;
     ~EndGameScene() override = default;
 };
 
---
+++ src/EndGameScene.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "ExitScene.h"
+#include "utils.h"
+#include "SaveMatchScene.h"
+#include "EndGameScene.h"
+#include "MainMenuScene.h"
+#include "TwoPlayerScene.h"
+
+
+EndGameScene::EndGameScene(Scene* lastScene, SDL_Renderer* renderer, int width, int height, std::string filename, bool whiteWon, bool blackWon) : Scene(lastScene, renderer, width, height, filename){
+    this->whiteWon = whiteWon;
+    this->blackWon = blackWon;
+}
+
+void EndGameScene::render()
+{
+
+    if(whiteWon){
+        importImageInRender(renderer, "assets/images/WhiteWins.png", 0, 0, width, height);
+    }else if(blackWon){
+        importImageInRender(renderer, "assets/images/BlackWins.png", 0, 0, width, height);
+    }else{
+        importImageInRender(renderer, "assets/images/Stalemate.png", 0, 0, width, height);
+    }
+
+    importImageInRender(renderer, "assets/images/close.png", width - width / 50 - width / 12, height / 50, width / 12, width / 12);
+    importImageInRender(renderer, "assets/images/PlayAgain.png", width / 3 - width / 24, height * 2 / 3 - width / 24, width / 12, width / 12);
+    importImageInRender(renderer, "assets/images/MainMenu.png", width * 2 / 3 - width / 24, height * 2 / 3 - width / 24, width / 12, width / 12);
+    SDL_RenderPresent(renderer);
+}
+
+Scene* EndGameScene::HandleEvent(SDL_Point click)
+{
+int mx = click.x;
+int my = click.y;
+
+if (mx == -1 && my == -1)
+{
+    return nullptr;
+}
+
+if (mx >= width - width / 50 - width / 12 && mx <= width - width / 50 && my >= height / 50 && my <= height / 50 + width / 12) {
+    return nullptr;
+}
+else if (mx >= width / 3 - width / 24 && mx <= width / 3 - width / 24 + width / 12 && my >= height * 2 / 3 - width / 24 && my <= height * 2 / 3 - width / 24 + width / 12) {
+    return new TwoPlayerScene(this, renderer, width, height, filename);
+}
+else if (mx >= width * 2 / 3 - width / 24 && mx <= width * 2 / 3 - width / 24 + width / 12 && my >= height * 2 / 3 - width / 24 && my <= height * 2 / 3 - width / 24 + width / 12) {
+    return new MainMenuScene(this, renderer, width, height, filename);
+}
+
+return this;
+}
+
